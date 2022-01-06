95fbd901e22ad254c0fb429078b1740f892170a1
Samuel Panek <samuelrpanek@gmail.com>
2022-01-05 17:11:07 -0800

    adds the snacks app to snacks project

---
+++ snacks/tests.py
@@ -0,0 +1,30 @@
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from snacks.views import HomePageView
+
+class SnacksTestCase(SimpleTestCase):
+  def test_home_page_status_code(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_home_page_template(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response,'home.html')
+
+  def test_about_page_status_code(self):
+    url = reverse('about')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_about_page_template(self):
+    url = reverse('about')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response,'about.html')
+
+
+
+# Create your tests here.
